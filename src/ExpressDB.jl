1b7e30337d65f2e47570f5324b19ac6c2e747c5a
singularitti <singularitti@outlook.com>
2022-12-13 00:50:33 -0600

    Add types `Data` & `Material`

--- src/ExpressDB.jl
+++ src/ExpressDB.jl
@@ -1,5 +1,17 @@
 module ExpressDB
 
-# Write your package code here.
+using ExpressBase: Calculation, SelfConsistentField
+using UUIDs: UUID
+
+abstract type Data end
+struct Material <: Data
+    id::UUID
+    formula::String
+    structure::Cell
+    pointgroup::Int
+    spacegroup::Int
+    nsites::Int
+    phase::String
+end
 
 end
