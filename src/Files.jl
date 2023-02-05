ce09b7d136d85229dfa6ea0417e4e6925c00e76a
singularitti <singularitti@outlook.com>
2023-02-04 19:10:33 -0500

    Add `listfiles` in src/Files.jl

--- src/Files.jl
+++ src/Files.jl
@@ -1,12 +1,23 @@
 # module Files
 
+export listfiles
+
 using Configurations: @option
+using FileTrees: FileTree, Path, files
 using Glob: GlobMatch
 
 @option struct ExpressFileTree
     root_dir::String
     input_pattern::Union{String,Regex,GlobMatch}
     output_pattern::Union{String,Regex,GlobMatch}
+end
+
+function listfiles(patterns, root_dir=pwd())
+    tree = FileTree(root_dir)
+    return map((patterns.first, patterns.second)) do pattern
+        subtree = tree[pattern]
+        abspath.(Path.(files(subtree)))
+    end
 end
 
 # end
