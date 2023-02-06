844e050e790bde03dfad828cb5b1f8def7fbbe79
singularitti <singularitti@outlook.com>
2023-02-06 02:38:41 -0500

    Move `Data` types to data.jl

---
+++ src/data.jl
@@ -0,0 +1,35 @@
+using ChemicalFormula: Formula
+using CrystallographyBase: Cell, MonkhorstPackGrid
+using EquationsOfStateOfSolids: EquationOfStateOfSolids
+using Pseudopotentials: ExchangeCorrelationFunctional, Pseudization
+using UUIDs: UUID
+
+export Crystal, SimpleScf, EosFitting, PhononDos
+
+abstract type Data end
+abstract type MaterialsData <: Data end
+struct Crystal <: MaterialsData
+    id::UUID
+    formula::Formula
+    structure::Cell
+    pointgroup::String
+    spacegroup::UInt16
+    nsites::UInt64
+end
+abstract type CalculationData <: Data end
+struct SimpleScf <: CalculationData
+    id::UUID
+    ecutwfc::Number
+    kmesh::MonkhorstPackGrid
+    xc::ExchangeCorrelationFunctional
+    pseudization::Pseudization
+end
+struct EosFitting <: CalculationData
+    input_eos::EquationOfStateOfSolids
+    final_eos::EquationOfStateOfSolids
+end
+struct PhononDos <: CalculationData
+    id::UUID
+    qmesh::MonkhorstPackGrid
+    dosmesh::MonkhorstPackGrid
+end
