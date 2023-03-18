e9d086a789285fc4a1426a652a5168a2da4918ba
singularitti <singularitti@outlook.com>
2023-03-18 01:32:18 -0400

    Add "Test distances between formulae"

--- test/distances.jl
+++ test/distances.jl
@@ -3,3 +3,9 @@
     y = Dict('a' => 8, 'c' => 3, 'e' => 8)
     @test distance(x, y) == 9
 end
+
+@testset "Test distances between formulae" begin
+    x = Formula("H2O")
+    y = Formula("H2S")
+    @test distance(x, y) == √2
+end
