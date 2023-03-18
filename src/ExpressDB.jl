f179b4ce45cba9580046c7afe48d1117af29221d
singularitti <singularitti@outlook.com>
2023-03-18 01:24:47 -0400

    Add `distance` for 2 `Dict`s in src/distances.jl

--- src/ExpressDB.jl
+++ src/ExpressDB.jl
@@ -2,6 +2,7 @@ module ExpressDB
 
 include("data.jl")
 include("table.jl")
+include("distances.jl")
 include("Files.jl")
 include("Reports.jl")
 
---
+++ src/distances.jl
@@ -0,0 +1,12 @@
+function distance(x::AbstractDict, y::AbstractDict)
+    keys₁, keys₂ = keys(x), keys(y)
+    common_keys = keys₁ ∩ keys₂
+    distinct_keys = symdiff(keys₁, keys₂)
+    # Euclidean distance for common keys
+    distance = sum(abs2, x[key] - y[key] for key in common_keys)
+    # Sum of absolute differences for non-overlapping keys
+    for key in distinct_keys
+        distance += key in x ? x[key]^2 : y[key]^2
+    end
+    return sqrt(distance)
+end
